5e5af9c04cbb8c08e3d9176d59bc882edf0e5e0f
tosnami <ahmedtosnami@gmail.com>
2025-02-16 02:48:45 +0200

    donee

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
-  return (
-      <div>
-          <WelcomeMessage />
-      </div>
-  );
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
