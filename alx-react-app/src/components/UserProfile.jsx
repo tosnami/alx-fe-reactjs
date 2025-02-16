53c4f72a08fb05c04bd53b89fec851442c7056c9
tosnami <ahmedtosnami@gmail.com>
2025-02-16 03:49:35 +0200

    donee

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
-function UserProfile( name, age, bio ) {
+import PropTypes from 'prop-types';
+
+const UserProfile = (props) => {
     return (
-        <div>
-            <h2>{name}</h2>
-            <p>Age: {age}</p>
-            <p>Bio: {bio}</p>
-        </div>
-    );
-}
+    <div>
+    <h2>{props.name}</h2>
+    <p>Age: {props.age}</p>
+    <p>Bio: {props.bio}</p>
+    </div>
+);
+};
+
+// Define prop types
+UserProfile.propTypes = {
+name: PropTypes.string.isRequired,
+age: PropTypes.number.isRequired,
+bio: PropTypes.string.isRequired,
+};
 
 export default UserProfile;
