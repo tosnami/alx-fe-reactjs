fdb098df1b2e93c5574905f557826d426283c948
tosnami <ahmedtosnami@gmail.com>
2025-02-23 04:41:29 +0200

    donee

--- alx-react-app-new/alx-react-app/src/components/MainContent.jsx
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -1,10 +1,10 @@
 function MainContent() {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'left' }}>
-      <h2>Welcome to My Favorite Cities</h2>
-      <p>Explore the most amazing places in the world.</p>
-    </main>
-  );
-}
-
-export default MainContent;
+    return (
+      <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'left' }}>
+        <h2>Welcome to My Favorite Cities</h2>
+        <p>Explore the most amazing places in the world.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
