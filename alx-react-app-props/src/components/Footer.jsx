05fa0a9e55e785ed5841fc3c101254a5e6165a03
tosnami <ahmedtosnami@gmail.com>
2025-02-23 05:13:53 +0200

    donne

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+        <p>© 2025 My Favorite Cities</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
