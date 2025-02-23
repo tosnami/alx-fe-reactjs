63ebeb8af4bd9f52585ca9ed3b0b47982cb89270
tosnami <ahmedtosnami@gmail.com>
2025-02-23 04:24:14 +0200

    donee

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,20 +1,26 @@
 import PropTypes from 'prop-types';
 
 const UserProfile = (props) => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
-};
+  return (
+    <div style={{
+      border: '1px solid gray',
+      padding: '10px',
+      margin: '10px',
+      borderRadius: '10px',
+      boxShadow: '4px 4px 10px rgba(0,0,0,0.1)',
+      textAlign: 'center'
+    }}>
+      <h2 style={{ color: 'blue', fontSize: '1.5em' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', color: 'darkred' }}>{props.age}</span></p>
+      <p style={{ fontStyle: 'italic', fontSize: '1.1em' }}>Bio: {props.bio}</p>
+    </div>
+  );
+}; 
 
-// تعريف PropTypes للتحقق من نوع البيانات
 UserProfile.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired,
-    bio: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  bio: PropTypes.string.isRequired
 };
 
-export default UserProfile;
+export default UserProfile; 
