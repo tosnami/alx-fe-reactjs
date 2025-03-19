d8d0252d9a5a71de99222640d32fb03db4f5f09e
tosnami <ahmedtosnami@gmail.com>
2025-03-19 21:41:49 +0200

    ddd

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-green-500 text-2xl font-bold text-center mt-10">
+    <div className="text-yellow-500 text-2xl font-bold text-center mt-10">
       Recipe Sharing Platform
     </div>
   );
