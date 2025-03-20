6ec6d63741515b826e5df404dbc921f6f7b1d168
tosnami <ahmedtosnami@gmail.com>
2025-03-20 04:51:51 +0200

    dd

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,27 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-      </Routes>
+      <div>
+        <header className="bg-blue-500 text-white p-4">
+          <h1 className="text-3xl font-bold">Recipe Sharing Platform</h1>
+        </header>
+        <main className="p-4">
+          <Routes>
+            {/* Home Page route */}
+            <Route path="/" element={<HomePage />} />
+            {/* Recipe Detail page route */}
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            {/* Add Recipe Form route */}
+            <Route path="/add-recipe" element={<AddRecipeForm />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
