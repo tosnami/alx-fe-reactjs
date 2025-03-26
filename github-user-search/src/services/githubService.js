f75d26c065c97a95c2bc5627da701d80cd8c5abe
tosnami <ahmedtosnami@gmail.com>
2025-03-26 08:02:01 +0200

    dd

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import axios from 'axios';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
