344af57b6f3ac077ad10edd592ffe7e427781809
tosnami <ahmedtosnami@gmail.com>
2025-07-31 20:23:28 +0300

    ddd

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,9 +2,9 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username: Yup.string().required("اسم المستخدم مطلوب"),
-  email: Yup.string().email("البريد الإلكتروني غير صالح").required("البريد الإلكتروني مطلوب"),
-  password: Yup.string().min(6, "كلمة المرور يجب أن تكون 6 أحرف على الأقل").required("كلمة المرور مطلوبة"),
+  username: Yup.string().required("name of user  "),
+  email: Yup.string().email("gmial is unsuccseful   ").required("needed gemail  "),
+  password: Yup.string().min(6, " password lrss than 6 characters").required("needed password"),
 });
 
 const FormikForm = () => {
@@ -12,24 +12,24 @@ const FormikForm = () => {
     <Formik
       initialValues={{ username: "", email: "", password: "" }}
       validationSchema={validationSchema}
-      onSubmit={(values) => console.log("تم إرسال البيانات:", values)}
+      onSubmit={(values) => console.log("sucsseful  :", values)}
     >
       {({ isSubmitting }) => (
         <Form>
-          <h2>تسجيل حساب</h2>
+          <h2> creqate account</h2>
           <div>
-            <Field type="text" name="username" placeholder="اسم المستخدم" />
+            <Field type="text" name="username" placeholder="name " />
             <ErrorMessage name="username" component="p" style={{ color: "red" }} />
           </div>
           <div>
-            <Field type="email" name="email" placeholder="البريد الإلكتروني" />
+            <Field type="email" name="email" placeholder="gemail " />
             <ErrorMessage name="email" component="p" style={{ color: "red" }} />
           </div>
           <div>
-            <Field type="password" name="password" placeholder="كلمة المرور" />
+            <Field type="password" name="password" placeholder=" password" />
             <ErrorMessage name="password" component="p" style={{ color: "red" }} />
           </div>
-          <button type="submit" disabled={isSubmitting}>تسجيل</button>
+          <button type="submit" disabled={isSubmitting}>login</button>
         </Form>
       )}
     </Formik>
--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      // 👇 يسمح بكتابة JSX داخل ملفات .js
+      babel: {
+        plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]]
+      }
+    })
+  ]
+});
